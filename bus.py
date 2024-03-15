274671ff640c0e1ea8e61bfc9a2253438d75eab7
SURYA1804 <suryaprakashm246@gmail.com>
2024-03-15 19:00:11 +0530

    In booked class only seat and bus id

--- bus.py
+++ bus.py
@@ -1,6 +1,3 @@
-#inlcude the command line and pushed through git
-#Seat is a class which consist of seat description.
-#gghhg
 class Seat:
     def __init__(self,type,price,place,id):
         self.type=type
@@ -8,33 +5,39 @@ class Seat:
         self.place=place
         self.id=id
 class Bus:
-    def __init__(self,name,type,price,place,id):
-        self.name=name
-        self.s=Seat(type,price,place,id)
-bus=[Bus("komban","semi_sleeper",1500,"window seat","KSSW"),Bus("komban","semi_sleeper",1500,"corner seat","KSSC"),Bus("komban","sleeper",2000,"window seat","KSW"),Bus("komban","sleeper",1500,"corner seat","KSC"),Bus("SRM","semi_sleeper",1600,"window seat","SSSW"),Bus("SRM","semi_sleeper",1600,"corner seat","SSSC"),Bus("SRM","sleeper",2100,"window seat","SSW"),Bus("SRM","sleeper",2100,"corner seat","SSC")]
-class Booked:
-    def __init__(self,bus_name,seat_type,seat_price,seat_place,id):
-        self.bus_name=bus_name
-        self.seat_type=seat_type
-        self.seat_price=seat_price
-        self.seat_place=seat_place
+    def __init__(self,id,name,type,price,place,seat_id):
         self.id=id
+        self.name=name
+        self.s=Seat(type,price,place,seat_id)
+bus=[Bus("K1","komban","semi_sleeper",1500,"window seat","SSW"),
+    Bus("K1","komban","semi_sleeper",1500,"corner seat","SSC"),
+    Bus("K1","komban","sleeper",2000,"window seat","SW"),
+    Bus("K1","komban","sleeper",1500,"corner seat","SC"),
+    Bus("S1","SRM","semi_sleeper",1600,"window seat","SSW"),
+    Bus("S1","SRM","semi_sleeper",1600,"corner seat","SSC"),
+    Bus("S1","SRM","sleeper",2100,"window seat","SW"),
+    Bus("S1","SRM","sleeper",2100,"corner seat","SC")]
+class Booked:
+    def __init__(self,bus_id,seat_id):
+        self.bus_id=bus_id
+        self.seat_id=seat_id
 booked=[]
 class discount:
-    def __init__(self,seat_type,seat_place,discount_rate):
-        self.seat_type=seat_type
-        self.seat_place=seat_place
+    def __init__(self,bus_id,seat_id,discount_rate):
+        self.seat_id=seat_id
+        self.bus_id=bus_id
         self.discount_rate=discount_rate
-d=[discount("sleeper","corner seat",5)]
+d=[discount("K1","SSW",5)]
 def calculation():
     for i in range(len(booked)):
-        if d[0].seat_type == booked[i].seat_type:
-            if d[0].seat_place == booked[i].seat_place:
-                return booked[i].seat_price-(booked[i].seat_price*d[0].discount_rate/100)
-            else:
-                return booked[i].seat_price
+        if booked[i].seat_id==d[0].seat_id and booked[i].bus_id==d[0].bus_id:
+            for j in range(len(bus)):
+                if booked[i].seat_id==bus[j].s.id and booked[i].bus_id==bus[j].id:
+                    return bus[j].s.price-(bus[j].s.price*d[0].discount_rate/100)
         else:
-            return booked[i].seat_price
+            for j in range(len(bus)):
+                if booked[i].seat_id==bus[j].s.id and booked[i].bus_id==bus[j].id:
+                    return bus[i].s.price
 
 
 
@@ -54,19 +57,20 @@ while True:
     if UserChoiceIsToViewTheSeats():
         print("******DISPLAYING THE SEATS********")
         for i in range(len(bus)):
-            print(f"BUS:{bus[i].name} || SEAT_ID: {bus[i].s.id} || SEAT_TYPE:{bus[i].s.type} || SEAT_PRICE: {bus[i].s.price} || SEAT_PLACE: {bus[i].s.place} ")
+            print(f"BUS:{bus[i].name} ||Bus_ID:{bus[i].id} || SEAT_ID: {bus[i].s.id} || SEAT_TYPE:{bus[i].s.type} || SEAT_PRICE: {bus[i].s.price} || SEAT_PLACE: {bus[i].s.place} ")
         print("-------------------------------------")
     elif UserChoiceIsToBookTheSeats():
+        buss=input("Enter the bus_id:")
         seat=input("Enter the Seat id :")
         c=0
         for j in range(len(booked)):
-            if seat==booked[j].id:
+            if seat==booked[j].seat_id and buss==booked[j].bus_id:
                 print("*******The Seat is already book**********")
                 c=c+1
         if c==0:
             for i in range(len(bus)):
-                if seat==bus[i].s.id:
-                    booked.append(Booked(bus[i].name,bus[i].s.type,bus[i].s.price,bus[i].s.place,bus[i].s.id))
+                if seat==bus[i].s.id and buss==bus[i].id:
+                    booked.append(Booked(bus[i].id,bus[i].s.id))
                     print("After Discount You have to PAY:",calculation())
                     print("*******BOOKING CONFORMIED***********")
                 else:
@@ -78,7 +82,7 @@ while True:
             print(" STILL  NO SEATS IS BOOKED ")
         else:
             for i in range(len(booked)):
-                print(f"seat_id:{booked[i].id}")
+                print(f"bus_id:{booked[i].bus_id} || SEAT_ID:{booked[i].seat_id}")
             print("*****************************")
     elif UserChoiceIsToQuit():
         break
